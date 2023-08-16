65dceac5326089453b660f89a89f64dead3f002f
NaveenDvlpr <naveenmoyya01@gmail.com>
2023-08-16 23:15:39 +0530

    C:/Program Files/Git/public

--- myApp.js
+++ myApp.js
@@ -5,12 +5,13 @@ let app = express();
     res.send('Hello Express');
 }) */
 
+app.use('/public', express.static(__dirname + '/public'));
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-app.use(express.static(__dirname + '/public'));
 
 console.log("Hello World");
 
